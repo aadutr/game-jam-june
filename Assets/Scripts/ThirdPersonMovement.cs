d3c4e8f05e9bd7e1f2d541c7b726db9469b0d851
aadutr <anouk.dutree@dutchanalytics.com>
2021-06-26 15:44:29 +0200

    updated player controller so it can now jump and added simple level environment

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    // public Animator animator;
+    public Transform cam;
+
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    [SerializeField]
+    private float jumpSpeed, gravity;
+
+    private bool playerGrounded;
+    private Vector3 jumpDirection = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f){
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+
+        playerGrounded = controller.isGrounded;
+
+        //jumping
+        if(Input.GetButton("Jump") && playerGrounded)
+        {
+            jumpDirection.y = jumpSpeed;
+        }
+        jumpDirection.y -= gravity * Time.deltaTime;
+
+        controller.Move(jumpDirection * Time.deltaTime);
+
+        //animations
+        // animator.SetBool("isRunning", Input.GetAxisRaw("Vertical") != 0);
+        // animator.SetBool("isJumping", !controller.isGrounded);
+    }
+}
